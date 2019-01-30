ca34e0378f7bf1703de4e1ad9389ffa5b90bb5ca
Tobias Frenger <Tobias-Frenger@users.noreply.github.com>
2019-01-30 14:06:39 +0100

    Inprogress updates - from laptop

---
+++ UDPChat/src/Server/Message.java
@@ -0,0 +1,43 @@
+package Server;
+
+import java.io.IOException;
+import java.net.DatagramSocket;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Message {
+	String message;
+	String name;
+	DatagramSocket socket;
+	ArrayList<ClientConnection> cc;
+	
+	public Message(
+			String message, 
+			String name, 
+			DatagramSocket socket, 
+			ArrayList<ClientConnection> cc) 
+	{
+		this.message = message;
+		this.name = name;
+		this.socket = socket;
+		this.cc = cc;
+	}
+	
+	public void sendPrivateMessage() throws IOException {
+		ClientConnection c;
+		for (Iterator<ClientConnection> itr = cc.iterator(); itr.hasNext();) {
+			c = itr.next();
+			if (c.hasName(name)) {
+				c.sendMessage(message, socket);
+			}
+		}
+	}
+	
+	public void broadcast() throws IOException {
+		ClientConnection c;
+		for (Iterator<ClientConnection> itr = cc.iterator(); itr.hasNext();) {
+			c = itr.next();
+			c.sendMessage(message, socket);
+		}
+	}
+}
--- UDPChat/src/Server/Server.java
+++ UDPChat/src/Server/Server.java
@@ -40,6 +40,22 @@ public class Server {
 		m_socket = new DatagramSocket(portNumber);
 	}
 
+	private DatagramPacket retrieveMessage() {
+		byte[] messageByte = new byte[1024];
+		DatagramPacket dp = new DatagramPacket(messageByte, messageByte.length);
+		try {
+			m_socket.receive(dp);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return dp;
+	}
+
+	private String unmarshalMessage(DatagramPacket datap) {
+		String message = new String(datap.getData(), 0, datap.getLength());
+		return message;
+	}
+
 	private void listenForClientMessages() throws IOException {
 		System.out.println("Waiting for client messages... ");
 
@@ -52,86 +68,109 @@ public class Server {
 			// response message to client detailing whether it was successful
 			// - Broadcast the message to all connected users using broadcast()
 			// - Send a private message to a user using sendPrivateMessage()
-			
-			//Retrieve message from client
-			byte[] messageByte = new byte[1024];
-			DatagramPacket dp = new DatagramPacket(messageByte, messageByte.length);
-			m_socket.receive(dp);
-			// Unmarshall message
-			String message = new String(dp.getData(), 0, dp.getLength());
-			
-			
-			ClientConnection c;
-			
+
+			// Retrieve message from client
+			DatagramPacket dp = retrieveMessage();
+			// Unmarshal message
+			String message = unmarshalMessage(dp);
+
 			// Retrieve the client name and put inside local string
-			String[] tempy = message.split("-name%");
-			String clientName = tempy[0];
+			String clientName = retrieveClientName(message);
+			// Removing name from message
 			message = message.replace("-name%", " -> ");
-			
+
 			// CONNECTED TO SERVER
-			if (message.contains("-connection%")) { 
-				String[] mes = message.split("-connection%");
-				addClient(mes[0], dp.getAddress(), dp.getPort());
-				for (Iterator<ClientConnection> itr = m_connectedClients.iterator(); itr.hasNext();) {
-					c = itr.next();
-					c.sendMessage(mes[0] + " connected to the server", m_socket);
-				}
+			if (message.contains("-connection%")) {
+				connectClientToServer(message, dp);
+				message = message.replaceAll(message, clientName + " has connected");
 			}
-			if (message.contains("/connect") && !checkIfConnected(clientName)) {
-				addClient(clientName, dp.getAddress(), dp.getPort());
-				message = message.replace(message, clientName + " has reconnected");
+			if (message.contains("/join") && !checkIfConnected(clientName)) {
+				message = reconnectClient(message, clientName, dp);
 			}
 			// Check if sender off message is a connected client
-			if (checkIfConnected(clientName)) {
-				// PRINT LIST OF USERS TO SELF
-				if (message.contains("/list")) { 
-					sendPrivateMessage("---Chat room users---", clientName);
-					for (Iterator<ClientConnection> itr = m_connectedClients.iterator(); itr.hasNext();) {
-						c = itr.next();
-						sendPrivateMessage("        - " + c.getName(), clientName);
-					}
-					sendPrivateMessage("-----------------------------", clientName);
-				} 
-				// SEND PRIVATE MESSAGE TO CLIENT
-				else if (message.contains("/tell ")) { 
-					for (Iterator<ClientConnection> itr = m_connectedClients.iterator(); itr.hasNext();) {
-						c = itr.next();
-						if (message.contains("/tell " + c.getName())) {
-							// RECIPENT MESSAGE
-							message = message.replace("/tell " + c.getName(), "");
-							message = message.replace("->", " whispers ->");
-							sendPrivateMessage(message, c.getName());
-							// SENDER MESSAGE
-							message = message.replace(clientName, "You");
-							message = message.replace("whispers", "whisper to " + c.getName());
-							sendPrivateMessage(message, clientName);
-						}
-					}
-				} 
-				// USER LEAVE CHAT
-				else if (message.contains("/leave")) {
-					message = message.replaceAll("/leave", "");
-					message = message.replace(clientName + " -> ", "");
-					if (!message.isEmpty()) {
-						broadcast(clientName + " final note: " + message);
-					}
-					disconnectClient(clientName);
-					m_socket.disconnect();
-				} 
-				// PRINT MESSAGE
-				else { 
-					broadcast(message);
-				}
+//			if (checkIfConnected(clientName)) {
+			// PRINT LIST OF USERS TO SELF
+			if (message.contains("/list")) {
+				printListOfUsers(clientName);
 			}
+			// SEND PRIVATE MESSAGE TO CLIENT
+			else if (message.contains("/tell ")) {
+				messageTell(message, clientName);
+			}
+			// USER LEAVE CHAT
+			else if (message.contains("/leave")) {
+				messageLeave(message, clientName);
+			}
+			// PRINT MESSAGE
+			else {
+				broadcast(message);
+			}
+//			}
 		} while (true);
 	}
+
+	private String retrieveClientName(String message) {
+		message = message.replace("-connection%", "");
+		String[] temp = message.split("-name%");
+
+		return temp[0];
+	}
+
+	private void connectClientToServer(String message, DatagramPacket datap) {
+		String[] mes = message.split("-connection%");
+		addClient(mes[0], datap.getAddress(), datap.getPort());
+		
+	}
 	
+	private String reconnectClient(String message, String name, DatagramPacket dp) {
+		addClient(name, dp.getAddress(), dp.getPort());
+		message = message.replace(message, name + " has reconnected");
+		return message;
+	}
+
+	private void printListOfUsers(String name) throws IOException {
+		ClientConnection c;
+		sendPrivateMessage("---Chat room users---", name);
+		for (Iterator<ClientConnection> itr = m_connectedClients.iterator(); itr.hasNext();) {
+			c = itr.next();
+			sendPrivateMessage("        - " + c.getName(), name);
+		}
+		sendPrivateMessage("-----------------------------", name);
+	}
+
+	private void messageLeave(String message, String name) throws IOException {
+		message = message.replaceAll("/leave", "");
+		message = message.replace(name + " -> ", "");
+		if (!message.isEmpty()) {
+			broadcast(name + " final note: " + message);
+		}
+		disconnectClient(name);
+	}
+
+	private void messageTell(String message, String name) throws IOException {
+		ClientConnection c;
+		for (Iterator<ClientConnection> itr = m_connectedClients.iterator(); itr.hasNext();) {
+			c = itr.next();
+			if (message.contains("/tell " + c.getName())) {
+				// RECIPENT MESSAGE
+				message = message.replace("/tell " + c.getName(), "");
+				message = message.replace("->", " whispers ->");
+				sendPrivateMessage(message, c.getName());
+				// SENDER MESSAGE
+				message = message.replace(name, "You");
+				message = message.replace("whispers", "whisper to " + c.getName());
+				sendPrivateMessage(message, name);
+			}
+		}
+	}
+
 	// Method to for the server to check if the sender is a connected client or not
 	private boolean checkIfConnected(String name) {
 		ClientConnection c;
 		for (Iterator<ClientConnection> itr = m_connectedClients.iterator(); itr.hasNext();) {
 			c = itr.next();
 			if (c.hasName(name)) {
+
 				return true; // client is connected
 			}
 		}
@@ -147,9 +186,30 @@ public class Server {
 			}
 		}
 		m_connectedClients.add(new ClientConnection(name, address, port));
-		System.out.println("Client added");
 		return true;
 	}
+	
+//	private void heartBeat() {
+//		Thread heartBeat = new HeartBeat() {
+//		public void run() {
+//			byte[] b = new byte[256];
+//			heartbeatMessage = new DatagramPacket(b, b.length, iadr, port);
+//			
+//			while(true) {
+//				try {
+//					socket.send(heartbeatMessage);
+//					sleep(heartBeatIntervallInMs);
+//				} catch (InterruptedException e) {
+//					// TODO Auto-generated catch block
+//					e.printStackTrace();
+//				} catch (IOException e) {
+//					// TODO Auto-generated catch block
+//					e.printStackTrace();
+//				}
+//			}
+//		};
+//		}
+//	}
 
 	public boolean disconnectClient(String name) throws IOException {
 		ClientConnection c;
@@ -159,6 +219,7 @@ public class Server {
 			if (c.hasName(name)) {
 				broadcast(name + " disconnected");
 				m_connectedClients.remove(c);
+				m_socket.disconnect();
 				return true;
 			}
 		}
